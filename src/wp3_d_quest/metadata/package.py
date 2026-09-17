669f270dc830443e32bf1a001fad2e910a2c8fdf
martonvago <57952344+martonvago@users.noreply.github.com>
2026-09-17 10:41:41 +0100

    docs: :memo: apply batched suggestions from code review
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- src/wp3_d_quest/metadata/package.py
+++ src/wp3_d_quest/metadata/package.py
@@ -6,7 +6,8 @@ package_properties = sp.SproutProperties(
     description=(
         "This repository contains the source raw data and Python code to build the "
         "data package for the DP-Next work package 3 population questionnaire. "
-        "Only metadata and documentation are publicly accessible."
+        "This package contains the data and metadata from a questionnaire sent out to residents in Denmark who had an HbA1c measurement taken in the last 24 months and who are between 40-50 years of age. The questionnaire study is part of the DP-Next project. "
+        "Only metadata and documentation are publicly accessible, personal data is kept within GenomeDK."
     ),
     homepage="https://dp-next.github.io/wp3-d-quest",
     contributors=[
@@ -42,6 +43,7 @@ package_properties = sp.SproutProperties(
             organization="Steno Diabetes Centre Aarhus",
             roles=["DataManager", "DataCurator"],
         ),
+        # TODO: Add other contributors, like David, Kristina, etc.
     ],
     licenses=[
         sp.LicenseProperties(
